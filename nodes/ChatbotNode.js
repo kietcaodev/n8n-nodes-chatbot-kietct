a2e5608e3a91072a9394b77cdefb3dd10b6496af
kietcaodev <71913797+kietcaodev@users.noreply.github.com>
2025-04-09 10:42:21 +0700

    test

---
+++ nodes/ChatbotNode.js
@@ -0,0 +1,39 @@
+class ChatbotNode {
+    description = {
+      displayName: 'Chatbot Node',
+      name: 'chatbotNode',
+      group: ['transform'],
+      version: 1,
+      description: 'Sends messages to chatbot',
+      defaults: {
+        name: 'Chatbot Node',
+        color: '#00aaff'
+      },
+      inputs: ['main'],
+      outputs: ['main'],
+      properties: [
+        {
+          displayName: 'Message',
+          name: 'message',
+          type: 'string',
+          default: '',
+          description: 'Message to send to the chatbot'
+        }
+      ]
+    };
+  
+    async execute() {
+      const items = this.getInputData();
+      const returnData = [];
+  
+      for (let i = 0; i < items.length; i++) {
+        const message = this.getNodeParameter('message', i);
+        returnData.push({ json: { message } });
+      }
+  
+      return this.prepareOutputData(returnData);
+    }
+  }
+  
+  module.exports = { ChatbotNode };
+  
